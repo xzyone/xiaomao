ec4d531f6e3f41451db782c970176d8e8bfc84b7
Yone <zhiyu@live.cn>
2026-09-21 13:23:53 +0800

    refactor: keep miniapp client on readonly error code only

--- miniprogram-project/services/request.js
+++ miniprogram-project/services/request.js
@@ -66,8 +66,7 @@ function buildHeaders(extra = {}) {
 }
 
 function isReadonlyModeResponse(statusCode, body = {}) {
-  return statusCode === 403 &&
-    (body.error === 'MINIAPP_READONLY' || body.error === 'MINIAPP_AUDIT_MODE')
+  return statusCode === 403 && body.error === 'MINIAPP_READONLY'
 }
 
 async function refreshSession() {
